33fa8a90914fd9d354ef250704d45614b73b35e5
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-01 21:25:27 +1000

    update to post

--- test/main.py
+++ test/main.py
@@ -9,11 +9,14 @@ pyodide_http.patch_all()
 def send_post(event):
 
     payload = { "foo": "bar" }
+    headers = {
+        "content-type": "application/json"
+    }
     url = "https://jsonplaceholder.typicode.com/posts/2"
     url = "https://webhook.site/b603beb6-d105-469c-91df-1c5f6e75fec0"
     url = "https://httpdump.app/dumps/179b653b-8119-4401-aa8a-2659ce265d5d"
-    #response = requests.post(url, data=payload)
-    response = requests.get(url)
+    response = requests.post(url, data=payload, headers=headers)
+    #response = requests.get(url)
     output_div = document.querySelector("#output")
     output_div.innerText += str(response.status_code)
     output_div.innerText += str(response.json())
