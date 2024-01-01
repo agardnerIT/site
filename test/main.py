4d14d4fc462361bd7723468dbba719c14cb61c0f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-01-01 21:21:09 +1000

    update url

--- test/main.py
+++ test/main.py
@@ -11,6 +11,7 @@ def send_post(event):
     payload = { "foo": "bar" }
     url = "https://jsonplaceholder.typicode.com/posts/2"
     url = "https://webhook.site/b603beb6-d105-469c-91df-1c5f6e75fec0"
+    url = "https://httpdump.app/dumps/179b653b-8119-4401-aa8a-2659ce265d5d"
     #response = requests.post(url, data=payload)
     response = requests.get(url)
     output_div = document.querySelector("#output")
